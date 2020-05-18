4885151c93d421e5c12d9827c85380a711275440
TakeaimK <44010902+TakeaimK@users.noreply.github.com>
2020-05-18 18:23:51 +0900

    (add) set up shared code that can be used in the other tests.

--- Hello_world/hello_test.go
+++ Hello_world/hello_test.go
@@ -5,21 +5,26 @@ import (
 )
 
 func TestHello(t *testing.T) {
+
+	//에러 발생 시 출력되는 공유 코드
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
 	t.Run("Say my name", func(t *testing.T) {
 		got := Hello("Sejin")
 		want := "Hello, Sejin"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 	t.Run("Say 'Hello world", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, world"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 }
