fabc4c669c2c4a27e89e3c0e0ddf419f61512881
TakeaimK <44010902+TakeaimK@users.noreply.github.com>
2020-05-19 23:20:04 +0900

    (mod) func SumAll : use "append"

--- Arrays_and_slices/sum.go
+++ Arrays_and_slices/sum.go
@@ -9,21 +9,9 @@ func Sum(numbers []int) int {
 }
 
 func SumAll(numbersToSum ...[]int) []int {
-	lengthOfNumbers := len(numbersToSum)
-	sums := make([]int, lengthOfNumbers)
-
-	for _, numbers := range numbersToSum {
-		sums = append(sums, Sum(numbers))
-	}
-
-	return sums
-}
-
-func SumAllTails(numbersToSum ...[]int) []int {
 	var sums []int
 	for _, numbers := range numbersToSum {
-		tail := numbers[1:]
-		sums = append(sums, Sum(tail))
+		sums = append(sums, Sum(numbers))
 	}
 
 	return sums
