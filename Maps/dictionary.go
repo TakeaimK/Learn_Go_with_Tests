fea0fa3ae0ed3bf87de82311154065eca92538f7
TakeaimK <44010902+TakeaimK@users.noreply.github.com>
2020-05-20 23:39:01 +0900

    (add) func Add

--- Maps/dictionary.go
+++ Maps/dictionary.go
@@ -1,7 +1,23 @@
 package main
 
+import "errors"
+
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
+type dict interface {
+	Search() (string, error)
+}
+
 type Dictionary map[string]string
 
-func Search(d Dictionary, word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+	def, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+	return def, nil
+}
+
+func (d Dictionary) Add(word, def string) {
+	d[word] = def
 }
