936fb6aaa5b3101c38192e603c52bed904398bd5
TakeaimK <44010902+TakeaimK@users.noreply.github.com>
2020-05-20 13:47:46 +0900

    (mod) use "struct"

--- Struct_method_and_interfaces/shapes_test.go
+++ Struct_method_and_interfaces/shapes_test.go
@@ -1,9 +1,15 @@
-package iteration
+package structs
 
 import "testing"
 
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
 	want := 40.0
 
 	if got != want {
@@ -12,7 +18,8 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	got := Area(12.0, 6.0)
+	rectangle := Rectangle{12.0, 6.0}
+	got := Area(rectangle)
 	want := 72.0
 
 	if got != want {
