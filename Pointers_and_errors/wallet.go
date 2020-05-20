31acd0c71ab12a2debf26b3acfc5c1d20d5a1e49
TakeaimK <44010902+TakeaimK@users.noreply.github.com>
2020-05-20 14:44:15 +0900

    (add) wallet.go > type Stringer interface

--- Pointers_and_errors/wallet.go
+++ Pointers_and_errors/wallet.go
@@ -1,9 +1,19 @@
 package Pointers_and_errors
 
+import "fmt"
+
 type Bitcoin int
+
+type Stringer interface {
+	String() string
+}
 
 type Wallet struct {
 	balance Bitcoin
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
 
 func (w *Wallet) Deposit(amount Bitcoin) {
