5b6698aa7c22d002f7b92fc556c33a0f44d5abd7
TakeaimK <44010902+TakeaimK@users.noreply.github.com>
2020-05-22 22:05:40 +0900

    (add) use "channel"

--- Reflection/walk.go
+++ Reflection/walk.go
@@ -28,6 +28,10 @@ func walk(x interface{}, fn func(input string)) {
 		for _, key := range val.MapKeys() {
 			walkValue(val.MapIndex(key))
 		}
+	case reflect.Chan:
+		for v, ok := val.Recv(); ok; v, ok = val.Recv() {
+			walk(v.Interface(), fn)
+		}
 	}
 
 	for i := 0; i < numberOfValues; i++ {
