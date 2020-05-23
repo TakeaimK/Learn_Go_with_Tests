86bc3121be198c618db71b83bed3bbfe1f27f897
TakeaimK <44010902+TakeaimK@users.noreply.github.com>
2020-05-23 14:55:57 +0900

    (mod) store.cancelled

--- Context/server.go
+++ Context/server.go
@@ -3,14 +3,39 @@ package server
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 type Store interface {
 	Fetch() string
+	Cancel()
+}
+
+type SpyStore struct {
+	response  string
+	cancelled bool
+}
+
+type StubStore struct {
+	response string
+}
+
+func (s *StubStore) Fetch() string {
+	return s.response
+}
+
+func (s *SpyStore) Fetch() string {
+	time.Sleep(100 * time.Millisecond)
+	return s.response
+}
+
+func (s *SpyStore) Cancel() {
+	s.cancelled = true
 }
 
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		store.Cancel()
 		fmt.Fprint(w, store.Fetch())
 	}
 }
