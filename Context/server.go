86d1c22551028a5690ca8477e2939393063838e6
TakeaimK <44010902+TakeaimK@users.noreply.github.com>
2020-05-23 15:13:54 +0900

    (mod) making assertion method on spy

--- Context/server.go
+++ Context/server.go
@@ -3,17 +3,11 @@ package server
 import (
 	"fmt"
 	"net/http"
-	"time"
 )
 
 type Store interface {
 	Fetch() string
 	Cancel()
-}
-
-type SpyStore struct {
-	response  string
-	cancelled bool
 }
 
 type StubStore struct {
@@ -22,15 +16,6 @@ type StubStore struct {
 
 func (s *StubStore) Fetch() string {
 	return s.response
-}
-
-func (s *SpyStore) Fetch() string {
-	time.Sleep(100 * time.Millisecond)
-	return s.response
-}
-
-func (s *SpyStore) Cancel() {
-	s.cancelled = true
 }
 
 func Server(store Store) http.HandlerFunc {
