8a8d3faae2bbff3072f9134425e2aaefc18d0726
TakeaimK <44010902+TakeaimK@users.noreply.github.com>
2020-05-23 15:21:07 +0900

    (mod) "tells store to cancel work if request is cancelled

--- Context/server.go
+++ Context/server.go
@@ -20,7 +20,12 @@ func (s *StubStore) Fetch() string {
 
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		data, _ := store.Fetch(r.Context())
+		data, err := store.Fetch(r.Context())
+
+		if err != nil {
+			return // todo: log error however you like
+		}
+
 		fmt.Fprint(w, data)
 	}
 }
