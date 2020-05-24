41073de5cdf518f998d1f9b7421392a7c7108155
TakeaimK <44010902+TakeaimK@users.noreply.github.com>
2020-05-24 09:13:17 +0900

    (add) coordinates

--- Math/clockface.go
+++ Math/clockface.go
@@ -5,6 +5,22 @@ import (
 	"time"
 )
 
+// A Point represents a two dimensional Cartesian coordinate
+type Point struct {
+	X float64
+	Y float64
+}
+
+// SecondHand is the unit vector of the second hand of an analogue clock at time `t`
+// represented as a Point.
+func SecondHand(t time.Time) Point {
+	return Point{}
+}
+
 func secondsInRadians(t time.Time) float64 {
 	return math.Pi / (30 / (float64(t.Second())))
+}
+
+func secondHandPoint(t time.Time) Point {
+	return Point{0, -1}
 }
