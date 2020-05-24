183d7063117c4888a6fde6b214f7074ed2617a84
TakeaimK <44010902+TakeaimK@users.noreply.github.com>
2020-05-24 13:47:42 +0900

    (mod)add func hourHandPoint

--- Math/clockface.go
+++ Math/clockface.go
@@ -39,3 +39,7 @@ func hoursInRadians(t time.Time) float64 {
 	return (minutesInRadians(t) / 12) +
 		(math.Pi / (6 / float64(t.Hour()%12)))
 }
+
+func hourHandPoint(t time.Time) Point {
+	return angleToPoint(hoursInRadians(t))
+}
