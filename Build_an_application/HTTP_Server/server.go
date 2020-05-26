2bd841699d2ebe4a131e2b8770b3c361ee08daf7
TakeaimK <44010902+TakeaimK@users.noreply.github.com>
2020-05-26 10:47:16 +0900

    Add NewInMemoryPlayerStore

--- Build_an_application/HTTP_Server/server.go
+++ Build_an_application/HTTP_Server/server.go
@@ -58,3 +58,19 @@ func (p *PlayerServer) processWin(w http.ResponseWriter, player string) {
 	p.store.RecordWin(player)
 	w.WriteHeader(http.StatusAccepted)
 }
+
+func NewInMemoryPlayerStore() *InMemoryPlayerStore {
+	return &InMemoryPlayerStore{map[string]int{}}
+}
+
+type InMemoryPlayerStore struct {
+	store map[string]int
+}
+
+func (i *InMemoryPlayerStore) RecordWin(name string) {
+	i.store[name]++
+}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return i.store[name]
+}
